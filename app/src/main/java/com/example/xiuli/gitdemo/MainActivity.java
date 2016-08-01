307e0806a8495512359903b4139835dfc36d93c9
xiuli <1041643771@qq.com>
2016-08-01 17:50:05 +0800

    second commit

--- app/src/main/java/com/example/xiuli/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/xiuli/gitdemo/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.xiuli.gitdemo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
