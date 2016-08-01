1f4af6ed0115a363b81ae3e52e0c6ab30932be8e
xiuli <1041643771@qq.com>
2016-08-01 17:46:05 +0800

    first commit

--- app/src/main/java/com/example/xiuli/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/xiuli/gitdemo/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.xiuli.gitdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
